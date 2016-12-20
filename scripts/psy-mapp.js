f06a54a64814a1ea8b25fac68fbcb3c62c077867
alexandr.nikienko <alexandr.nikienko@gmail.com>
2016-12-21 01:48:52 +0200

    psyMapp - worked version :)

--- scripts/psy-mapp.js
+++ scripts/psy-mapp.js
@@ -1,77 +1,52 @@
 var psyMapp = {
-    //IDs: ["1724395051210095", "559379634259373", "229116730823471", "181659822248251"],
-    //IDs: ["1724395051210095"],
-    //festivals: [],
+    IDs: ["1724395051210095", "559379634259373", "229116730823471", "181659822248251", "1361585657192075"],
     init: function () {
         console.log("Init Psy-Mapp");
-        //this.getData();
-        this.getLocalData();
+        this.initGoogleMap();
+        this.getData();
+        //this.getLocalData();
+
     },
     getLocalData: function() {
         $.getJSON( "data/data.json", function( data ) {
-            psyMapp.festivals = [];
             $.each( data, function( key, val ) {
-                psyMapp.getFestivalInfo(val);
                 psyMapp.setData(val);
+                psyMapp.setMarker(psyMapp.map, val);
             });
-            psyMapp.initGoogleMap(psyMapp.festivals);
         });
     },
     getData: function (id) {
-        var IDs = ["1724395051210095", "181659822248251"];
         psyMapp.festivals = [];
-        for (var i = 0; i < IDs.length; i++) {
+        for (var i = 0; i < psyMapp.IDs.length; i++) {
             FB.api(
-                '/' + IDs[i],
+                '/' + psyMapp.IDs[i],
                 'GET',
                 {},
                 function(data) {
-                    console.log(data);
-                    psyMapp.getFestivalInfo(data);
+                    psyMapp.setMarker(psyMapp.map, data);
                     psyMapp.setData(data);
                 }
             )
         }
-        console.log("initGoogleMap with: ", psyMapp.festivals);
-        psyMapp.initGoogleMap(psyMapp.festivals);
-    },
-    getFestivalInfo: function(obj) {
-        var festival = [];
-        festival[0] = obj.name;
-        if (!obj.place.location) {
-            festival[1] = 0;
-        } else {
-            festival[1] = obj.place.location.latitude;
-        }
-        if (!obj.place.longitude) {
-            festival[2] = 0;
-        } else {
-            festival[2] = obj.place.location.longitude;
-        }
-        psyMapp.festivals.push(festival);
     },
     setData: function (data) {
         $("#results").append(JSON.stringify(data.name) + "<br>");
     },
     initGoogleMap: function(arr) {
-        var map = new google.maps.Map(document.getElementById('map'), {
+        psyMapp.map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 52.519325, lng: 13.392709},
             zoom: 5
         });
-        this.setMarkers(map, arr);
     },
-    setMarkers: function(map, festivals) {
-        console.log("markers for ", festivals);
-        console.log("length  ", festivals.length);
+    setMarker: function(map, obj) {
+        var lat = !obj.place.location ? 0 : obj.place.location.latitude,
+            lng = !obj.place.longitude ? 0 : obj.place.location.longitude;
 
-        for (var i = 0; i < festivals.length; i++) {
-            var festival = festivals[i];
-            console.log("marker for ", festival);
-            var marker = new google.maps.Marker({
-                position: {lat: festival[1], lng: festival[2]},
-                map: map,
-                title: festival[0]
-            });
-        }
+        var marker = new google.maps.Marker({
+            position: {lat: lat, lng: lng},
+            map: map,
+            title: obj.name
+        });
     }
 }
+
